1dc107a1d1a12a4ce9011a72b10b9cc08c182d03
Anmol Deep <87927563+anmoldeep38@users.noreply.github.com>
2024-06-03 01:33:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ dotenv.config()
 const app = express()
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin: "https://github.com/anmoldeep38/Quizee-backen/blob/main/app.js",
     credentials: true
 }))
 
