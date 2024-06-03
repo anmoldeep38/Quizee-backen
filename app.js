3fe873bb029966acd59af2e58d3c0038cb5dfa5f
Anmol Deep <87927563+anmoldeep38@users.noreply.github.com>
2024-06-03 15:08:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -24,10 +24,9 @@ import userRoutes from './routes/user.routes.js'
 import quizRoutes from './routes/quiz.routes.js'
 
 //routes config
-//app.use("/api/v1/account", userRoutes);
-//app.use("/api/v1/quiz", quizRoutes)
-app.use("/account", userRoutes);
-app.use("/quiz", quizRoutes)
+app.use("/api/v1/account", userRoutes);
+app.use("/api/v1/quiz", quizRoutes)
+
 
 // app.all("*", (req, res) => {
 //     res.status(404).json({
